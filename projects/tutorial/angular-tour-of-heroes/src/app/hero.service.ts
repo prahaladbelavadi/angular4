697d7d42c5641aee206be87f791c50ad8920cda9
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-09-19 15:09:13 +0530

    implement services ; next up routing

--- projects/tutorial/angular-tour-of-heroes/src/app/hero.service.ts
+++ projects/tutorial/angular-tour-of-heroes/src/app/hero.service.ts
@@ -11,7 +11,7 @@ import { MessageService } from './message.service'
 })
 export class HeroService {
 
-  constructor(private MessageService: MessageService){ }
+  constructor(private messageService: MessageService){ }
 
   getHeroes(): Observable<Hero[]>{
     // TODO: send the message _after_ fetching the heroes
